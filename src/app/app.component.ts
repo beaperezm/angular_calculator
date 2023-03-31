66230d036518086db53adb24f39ea85cd1f4ce9b
Carlos Minaya <bpml2001@hotmail.com>
2023-03-31 21:22:43 +0200

    feat(calculator): first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculator';
+  title = 'My Calculator';
+
+  public number1: number = 0;
+  public number2: number = 0;
+  public result?: number;
+  public form?: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      numbera: new FormControl('', [Validators.required]),
+      numberb: new FormControl('', [Validators.required])
+    })
+  }
+
+ public plus() {
+  this.number1 ++
+ }
+
+ public minus() {
+  this.number1 --
+ }
+
+ public plus2() {
+  this.number2 ++
+ }
+
+ public minus2() {
+  this.number2 --
+ }
+
+  public add(): void {
+    this.result = this.number1 + this.number2
+  };
+
+  public subs(): void {
+    this.result = this.number1 - this.number2
+  };
+
+  public mult(): void {
+    this.result = this.number1 * this.number2
+  }
+
+  public div(): void {
+    this.result = this.number1 / this.number2
+  
+  }
+
+  public clear(): void {
+    if(!this.result) { 
+          return; 
+        } else {
+          delete this.result && this.number1 === 0 && this.number2 === 0; 
+        }
+  }
+  
 }
